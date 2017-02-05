2ae1286f8fdd38d219697987a83ef8521b76c7ad
fabianterhorst <fabian.terhorst@gmail.com>
2017-02-05 22:36:59 +0100

    * complex layout test

--- app/src/main/java/io/fabianterhorst/layoutkit/sample/CustomView.java
+++ app/src/main/java/io/fabianterhorst/layoutkit/sample/CustomView.java
@@ -10,8 +10,10 @@ import android.widget.TextView;
 
 import java.util.Arrays;
 
+import io.fabianterhorst.layoutkit.Alignment;
 import io.fabianterhorst.layoutkit.Axis;
 import io.fabianterhorst.layoutkit.BaseView;
+import io.fabianterhorst.layoutkit.Flexibility;
 import io.fabianterhorst.layoutkit.layouts.SizeLayout;
 import io.fabianterhorst.layoutkit.layouts.StackLayout;
 
@@ -25,15 +27,29 @@ public class CustomView extends View {
 
     private TextView textView2 = new TextView(getContext());
 
+    private TextView textView3 = new TextView(getContext());
+
+    private TextView textView4 = new TextView(getContext());
+
     private BaseView baseView = new BaseView(textView);
 
     private BaseView baseView2 = new BaseView(textView2);
 
-    private SizeLayout sizeLayout = new SizeLayout(baseView, 100f, null, 100f, null, null, null, null, null);
+    private BaseView baseView3 = new BaseView(textView3);
 
-    private SizeLayout sizeLayout2 = new SizeLayout(baseView2, 100f, null, 100f, null, null, null, null, null);
+    private BaseView baseView4 = new BaseView(textView4);
 
-    private StackLayout stackLayout = new StackLayout(Axis.VERTICAL, 0, null, null, null, Arrays.asList(sizeLayout, sizeLayout2), null);
+    private SizeLayout sizeLayout = new SizeLayout(baseView, 1f, null, 100f, 100f, Alignment.fill, Flexibility.min, null, null);
+
+    private SizeLayout sizeLayout2 = new SizeLayout(baseView2, 1f, null, 100f, 100f, Alignment.fill, Flexibility.min, null, null);
+
+    private SizeLayout sizeLayout3 = new SizeLayout(baseView3, 1f, null, 100f, 100f, null, null, null, null);
+
+    private SizeLayout sizeLayout4 = new SizeLayout(baseView4, 1f, null, 100f, 100f, null, null, null, null);
+
+    private StackLayout stackLayout2 = new StackLayout(Axis.VERTICAL, 0, null, null, null, Arrays.asList(sizeLayout3, sizeLayout4), null);
+
+    private StackLayout stackLayout = new StackLayout(Axis.HORIZONTAL, 0, StackLayout.StackLayoutDistribution.FILL_EQUAL_SIZE, null, null, Arrays.asList(sizeLayout, sizeLayout2, stackLayout2), null);
 
     public CustomView(Context context) {
         super(context);
@@ -51,6 +67,10 @@ public class CustomView extends View {
     }
 
     private void init() {
+        textView4.setText("bla6");
+        textView4.setBackgroundColor(Color.CYAN);
+        textView3.setText("bla4");
+        textView3.setBackgroundColor(Color.DKGRAY);
         textView.setText("bla");
         textView.setBackgroundColor(Color.GRAY);
         textView2.setText("bla2");
