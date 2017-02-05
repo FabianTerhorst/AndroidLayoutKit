bf6c0140a88c460a87e4b56c8d01183298ab0d65
fabianterhorst <fabian.terhorst@gmail.com>
2017-02-05 21:37:10 +0100

    * update sample

--- app/src/main/java/io/fabianterhorst/layoutkit/sample/CustomView.java
+++ app/src/main/java/io/fabianterhorst/layoutkit/sample/CustomView.java
@@ -8,8 +8,12 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.Arrays;
+
+import io.fabianterhorst.layoutkit.Axis;
 import io.fabianterhorst.layoutkit.BaseView;
 import io.fabianterhorst.layoutkit.layouts.SizeLayout;
+import io.fabianterhorst.layoutkit.layouts.StackLayout;
 
 /**
  * Created by fabianterhorst on 03.02.17.
@@ -19,9 +23,17 @@ public class CustomView extends View {
 
     private TextView textView = new TextView(getContext());
 
+    private TextView textView2 = new TextView(getContext());
+
     private BaseView baseView = new BaseView(textView);
 
+    private BaseView baseView2 = new BaseView(textView2);
+
     private SizeLayout sizeLayout = new SizeLayout(baseView, 100f, null, 100f, null, null, null, null, null);
+
+    private SizeLayout sizeLayout2 = new SizeLayout(baseView2, 100f, null, 100f, null, null, null, null, null);
+
+    private StackLayout stackLayout = new StackLayout(Axis.VERTICAL, 0, null, null, null, Arrays.asList(sizeLayout, sizeLayout2), null);
 
     public CustomView(Context context) {
         super(context);
@@ -41,12 +53,14 @@ public class CustomView extends View {
     private void init() {
         textView.setText("bla");
         textView.setBackgroundColor(Color.GRAY);
+        textView2.setText("bla2");
+        textView2.setBackgroundColor(Color.BLUE);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        sizeLayout.arrangement(null, 200f, 200f).makeViews(null).draw(canvas);
+        stackLayout.arrangement(null, 200f, 200f).makeViews(null).draw(canvas);
     }
 
     @Override
