ce3493e214566e5e5b6026492882300d6e6dbe60
fabianterhorst <fabian.terhorst@gmail.com>
2017-02-13 16:05:23 +0100

    * update sample

--- app/src/main/java/io/fabianterhorst/layoutkit/sample/MainActivity.java
+++ app/src/main/java/io/fabianterhorst/layoutkit/sample/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import io.fabianterhorst.layoutkit.android.LayoutView;
+import io.fabianterhorst.layoutkit.utils.SizeLayoutBuilder;
+
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
@@ -14,7 +17,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new CustomView(this));
+        /*setContentView(
+                new LayoutView(this, new SizeLayoutBuilder()
+                        .baseView(new CustomBaseView())
+                        .size(100f, 100f)
+                        .build().arrangement().makeViews())
+        );*/
+        setContentView(
+                new LayoutView(this, new SizeLayoutBuilder()
+                        .baseView(new CustomBaseView())
+                        .size(100f, 100f)
+                        .build())
+        );
+        //setContentView(new CustomView(this)));
 
         // Example of a call to a native method
         //TextView tv = (TextView) findViewById(R.id.sample_text);
