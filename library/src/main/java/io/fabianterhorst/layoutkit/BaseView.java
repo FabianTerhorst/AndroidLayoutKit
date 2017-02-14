e93c59eeb8ca8ca030889208bb79e347ea1a0309
fabianterhorst <fabian.terhorst@gmail.com>
2017-02-14 12:47:45 +0100

    * add touch event support

--- library/src/main/java/io/fabianterhorst/layoutkit/BaseView.java
+++ library/src/main/java/io/fabianterhorst/layoutkit/BaseView.java
@@ -1,6 +1,7 @@
 package io.fabianterhorst.layoutkit;
 
 import android.graphics.Canvas;
+import android.view.MotionEvent;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -61,5 +62,23 @@ public class BaseView {
         if (subViews == null)
             subViews = new ArrayList<>();
         subViews.add(view);
+    }
+
+    protected void onTouchEvent(float x, float y, MotionEvent event) {
+    }
+
+    public void dispatchTouchEvent(MotionEvent event) {
+        float xStart = frame.getOrigin().getX();
+        float xEnd = xStart + frame.getWidth();
+        float yStart = frame.getOrigin().getY();
+        float yEnd = yStart + frame.getHeight();
+        if (event.getX() >= xStart && event.getX() <= xEnd
+                && event.getY() >= yStart && event.getY() <= yEnd) {
+            onTouchEvent(event.getX() - xStart, event.getY() - yStart, event);
+            if (subViews == null) return;
+            for (BaseView view : subViews) {
+                view.dispatchTouchEvent(event);
+            }
+        }
     }
 }
