efcc00bd24de826a8699c88cc3a9bef3542ef5a8
fabianterhorst <fabian.terhorst@gmail.com>
2017-02-14 12:47:57 +0100

    * update sample

--- app/src/main/java/io/fabianterhorst/layoutkit/sample/MainActivity.java
+++ app/src/main/java/io/fabianterhorst/layoutkit/sample/MainActivity.java
@@ -3,9 +3,17 @@ package io.fabianterhorst.layoutkit.sample;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import java.util.Arrays;
+
+import io.fabianterhorst.layoutkit.Axis;
+import io.fabianterhorst.layoutkit.android.AndroidBaseView;
 import io.fabianterhorst.layoutkit.android.LayoutView;
+import io.fabianterhorst.layoutkit.layouts.BaseLayout;
 import io.fabianterhorst.layoutkit.utils.SizeLayoutBuilder;
+import io.fabianterhorst.layoutkit.utils.StackLayoutBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,13 +31,55 @@ public class MainActivity extends AppCompatActivity {
                         .size(100f, 100f)
                         .build().arrangement().makeViews())
         );*/
-        setContentView(
+        /*setContentView(
                 new LayoutView(this, new SizeLayoutBuilder()
                         .baseView(new CustomBaseView())
                         .size(100f, 100f)
                         .build())
-        );
-        //setContentView(new CustomView(this)));
+        );*/
+        //setContentView(new CustomView(this));
+
+        setContentView(new LayoutView(this, new StackLayoutBuilder()
+                .axis(Axis.VERTICAL)
+                .subLayouts(Arrays.asList(new SizeLayoutBuilder()
+                        .baseView(new AndroidBaseView(new Button(this)))
+                        .size(1f, 200f)
+                        .layoutConfig(new BaseLayout.LayoutConfig() {
+                            @Override
+                            public void onConfigure(Object view) {
+                                if (view instanceof Button) {
+                                    Button button = (Button) view;
+                                    button.setText("bla");
+                                    button.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View view) {
+                                            Log.d("bla", stringFromJNI());
+                                        }
+                                    });
+                                }
+                            }
+                        })
+                        .build(), new SizeLayoutBuilder()
+                        .baseView(new AndroidBaseView(new Button(this)))
+                        .size(1f, 200f)
+                        .layoutConfig(new BaseLayout.LayoutConfig() {
+                            @Override
+                            public void onConfigure(Object view) {
+                                if (view instanceof Button) {
+                                    Button button = (Button) view;
+                                    button.setText("bla2");
+                                    button.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View view) {
+                                            Log.d("bla2", stringFromJNI());
+                                        }
+                                    });
+                                }
+                            }
+                        })
+                        .build()
+                ))
+                .build()));
 
         // Example of a call to a native method
         //TextView tv = (TextView) findViewById(R.id.sample_text);
